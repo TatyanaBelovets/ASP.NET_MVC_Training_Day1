2f84756014a944df298d4a39c89d2ed04828ed2e
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-09 12:10:37 +0300

    :(

---
+++ Task1.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.ConsoleCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rootByNewton = Task1.Library.Number.FindRootByNewton(9, 2, 0.00001);
+            double rootByMath = Math.Pow(9, 0.5);
+            Console.WriteLine("{0}, {1}, {2}", rootByMath, rootByNewton, Math.Abs(rootByMath - rootByNewton));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1.Library/Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Library
+{
+    public class Number
+    {
+        public static double FindRootByNewton(double number, int degree, double eps)
+        {
+            /*double n;
+    cin >> n;
+    const double EPS = 1E-15;//1E-15
+    double x = 1;
+    for (;;) 
+    {
+        double nx = 
+        if (abs (x - nx) < EPS)  break;
+        x = nx;
+    }
+    printf ("%.15lf", x);  */
+            double x = 1;
+            double xk = x - ((x - number) / (number * x));
+            while (Math.Abs(x - xk) > eps)
+            {
+                xk = xk - ((Math.Pow(xk, degree) - number)/(number*Math.Pow(xk, degree - 1)));
+                x = xk;
+            }
+            return x;
+        }
+    }
+}
