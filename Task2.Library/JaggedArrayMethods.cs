35fdca9b0341ab30bba0853d439c87597c902967
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-12 01:50:23 +0300

    Deleted useless functions from library, changed type of function

--- Task2.Library/JaggedArrayMethods.cs
+++ Task2.Library/JaggedArrayMethods.cs
@@ -1,12 +1,11 @@
-﻿using System.Linq;
+﻿using System;
 
 namespace Task2.Library
 {
     public static class JaggedArrayMethods
     {
-        public delegate bool SortingRuleDelegate(int[] a, int[] b);
-
-        public static int[][] Sort(int[][] array, SortingRuleDelegate sortingRule)
+        public static void Sort(int[][] array, Func<int[], int[], bool> sortingRule) //sortingRule is a delegate, 
+                                                                                     //parametres: <in int[], in int[], out bool>
         {
             for (var i = 0; i < array.Length; i++)
             {
@@ -19,37 +18,7 @@ namespace Task2.Library
                 array[i] = array[nMaxLine];
                 array[nMaxLine] = temp;
             }
-            return array;
         }
 
-        public static bool MaxElement(int[] a, int[] b)
-        {
-            return a.Max() > b.Max();
-        }
-
-        public static bool MinElement(int[] a, int[] b)
-        {
-            return a.Min() < b.Min();
-        }
-
-        public static bool SumElements(int[] a, int[] b)
-        {
-            return a.Sum() > b.Sum();
-        }
-
-        public static int[][] SortingByMaxElement(int[][] array)
-        {
-            return Sort(array, MaxElement);
-        }        
-        
-        public static int[][] SortingByMinElement(int[][] array)
-        {
-            return Sort(array, MinElement);
-        }        
-        
-        public static int[][] SortingBySumElements(int[][] array)
-        {
-            return Sort(array, SumElements);
-        }
     }
 }
