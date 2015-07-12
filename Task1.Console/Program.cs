ed9295c62ecac4eaa5d8ad5b5851a8635f0fd1bf
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-12 13:15:10 +0300

    added exception handling; changed accuracy of the method

--- Task1.Console/Program.cs
+++ Task1.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task1.Library;
 
 namespace Task1.ConsoleCheck
 {
@@ -10,9 +11,10 @@ namespace Task1.ConsoleCheck
     {
         static void Main(string[] args)
         {
-            double rootByNewton = Task1.Library.Number.FindRootByNewton(81, 4, 0.0001);
-            double rootByMath = Math.Pow(81, 0.25);
+            double rootByNewton = Number.FindRootByNewton(817, 4, 0.001);
+            double rootByMath = Math.Pow(817, 0.25);
             Console.WriteLine("{0}, {1}, {2}", rootByMath, rootByNewton, Math.Abs(rootByMath - rootByNewton));
+            //Number.FindRootByNewton(-817, 4, 0.001);
             Console.ReadKey();
         }
     }
--- Task1.Library/Number.cs
+++ Task1.Library/Number.cs
@@ -10,15 +10,20 @@ namespace Task1.Library
     {
         public static double FindRootByNewton(double number, int degree, double eps)
         {
+            if (number < 0 && degree % 2 == 0)
+            {
+                throw new ArgumentException("Root of even degree for negative numbers is undefined");
+            }
+
             double x = 1;
             double xk = 2 - (x / number);
             while (Math.Abs(x - xk) > eps)
             {
                 x = xk;
-                xk = xk - ((Math.Pow(xk, degree) - number)/(number*Math.Pow(xk, degree - 1)));
-                
+                xk = xk - xk / degree + number * Math.Pow(xk, 1 - degree) / degree;
             }
             return x;
         }
     }
 }
+
